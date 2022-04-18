1ec5880e7f51ddae61d162d51f4a5138f858497b
bmsani <bmsani6@gmail.com>
2022-04-18 18:16:20 +0600

    checkout form added

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Checkout = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+    }
     return (
-        <div>
-            Checkout Page
+        <div className='my-5'>
+        <h1 className='text-primary text-center my-3'>Checkout</h1>
+            <Form className='w-50 d-block mx-auto'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control type="text" placeholder="Your name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control type="number" placeholder="Phone" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control style={{height: '80px'}} type="text" placeholder="Address" />
+                </Form.Group>
+                <Button className='rounded-pill d-block mx-auto w-25' onClick={handleSubmit} variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
